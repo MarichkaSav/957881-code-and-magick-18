41739548ba6bc105fce62074807f0453744f8d35
Мария Савчук <marichka.sav@gmail.com>
2019-09-23 08:46:16 +0300

    Внесены правки по циклам

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,33 @@
 'use strict';
 
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 200;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 50;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+
+var getRandomPercentage = function () {
+  return Math.floor(Math.random() * 101);
+};
+var getBlueWithRandomSaturation = function () {
+  return 'hsl(240, ' + getRandomPercentage() + '%, 50%)';
+};
+
+var getMaxElement = function(times) {
+  var maxElement = times[0];
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > maxElement) {
+      maxElement = times[i];
+    }
+  }
+};
+
+var maxTime = getMaxElement(times);
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -13,20 +41,35 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 230, 20);
   ctx.fillText('Список результатов:', 220, 40);
 
+  var printNames = function (names) {
+    for (var i = 0; i < names; i++) {
+      if (printNames === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = getBlueWithRandomSaturation();
+      }
+    }
+  };
 
-  ctx.fillText('Вы', 150, 250);
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(150, 90, 40, 150);
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+  }
 
-  ctx.fillText('Кекс', 240, 250);
-  ctx.fillStyle = 'hsl(240, ' + Math.random() + ', 50)';
-  ctx.fillRect(240, 90, 40, 150);
 
-  ctx.fillText('Катя', 330, 250);
-  ctx.fillStyle = 'hsl(240, ' + Math.random() + ', 50)';
-  ctx.fillRect(330, 90, 40, 150);
+  // ctx.fillText('Вы', 150, 250);
+  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  // ctx.fillRect(150, 90, 40, 150);
 
-  ctx.fillText('Игорь', 420, 250);
-  ctx.fillStyle = 'hsl(240, ' + Math.random() + ', 50)';
-  ctx.fillRect(420, 90, 40, 150);
+  // ctx.fillText('Кекс', 240, 250);
+  // ctx.fillStyle = getBlueWithRandomSaturation();
+  // ctx.fillRect(240, 90, 40, 150);
+
+  // ctx.fillText('Катя', 330, 250);
+  // ctx.fillStyle = getBlueWithRandomSaturation();
+  // ctx.fillRect(330, 90, 40, 150);
+
+  // ctx.fillText('Игорь', 420, 250);
+  // ctx.fillStyle = getBlueWithRandomSaturation();
+  // ctx.fillRect(420, 90, 40, 150);
 };
